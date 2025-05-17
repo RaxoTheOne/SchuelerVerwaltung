c7a632f72b3630551b8959357d6bc62ce008c7a5
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-05-17 15:24:44 +0200

    Projektdateien hinzufügen.

---
+++ SchuelerVerwaltung/Schueler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Schueler
+{
+	public string name;
+	public int alter;
+	public string klasse;
+	public Schueler(string name, int alter, string klasse)
+	{
+		this.name = name;
+		this.alter = alter;
+		this.klasse = klasse;
+	}
+
+	public void Ausgabe()
+	{
+		Console.WriteLine($"Name: {name}, Alter: {alter}, Klasse: {klasse}");
+	}
+}
